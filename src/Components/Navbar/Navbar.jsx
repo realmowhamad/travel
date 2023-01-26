645e82cdb3cdffe2744e0d4eee6d162f09f2625d
Mohammad Mirhoseini <Realmowhamad@gmail.com>
2023-01-26 12:12:46 +0330

    Create Navbar Basic

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='w-full flex items-center justify-between'>
+            <div>Logo</div>
+            <div>SearchBar</div>
+            <div>user</div>
+        </div>
+    );
+}
+
+export default Navbar;
--- src/Pages/Mainpage/Main.jsx
+++ src/Pages/Mainpage/Main.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import Navbar from "../../Components/Navbar/Navbar";
 
 export default function Main() {
-    return <div className="bg-red-200 flex-grow">Main</div>;
+    return (
+        <div className="bg-red-200 flex-grow">
+            <Navbar />
+
+        </div>
+    )
 }
