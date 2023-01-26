853ef1714a0498a4588eeb7aa35f26ddb467b5ea
Mohammad Mirhoseini <Realmowhamad@gmail.com>
2023-01-26 14:23:07 +0330

    SearchBar feature UI complete

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,27 +4,32 @@ import { FiSearch, FiMenu } from 'react-icons/fi'
 
 const Navbar = () => {
     return (
-        <div className='w-full flex items-center justify-center sm:justify-between h-20 py-3 px-6 my-2 '>
-            <div className='hidden sm:block'>Logo</div>
-            <div className={`${style.searchBar} w-full sm:w-auto h-full bg-white flex items-center `}>
-                <i className='text-xl px-5'><FiSearch /></i>
-                <div className=' flex flex-col w-3/6'>
-                    <h1 className='font-bold text-lg'>Anywhere</h1>
-                    <div className='flex text-sm'>
-                        <p className='pr-2'>Any week</p>
-                        <p>Any guests</p>
+        <div className='w-full flex items-center justify-center sm:justify-between h-20 py-3 px-6 my-2 lg:justify-between '>
+            <div className='hidden lg:block'>Logo</div>
+
+            <div className={`${style.searchBar} w-full h-full bg-white flex items-center justify-start lg:flex-row-reverse lg:justify-between lg:w-4/12 lg:max-w-lg`}>
+
+                <i className='text-xl px-5 sm:-order-last lg:p-2 lg:bg-red-500 lg:rounded-full lg:mr-2'>
+                    <FiSearch />
+                </i>
+
+                <div className=' flex flex-col justify-start w-3/6 D  sm:w-full lg:w-full lg:items-center lg:justify-between lg:flex-row pl-5  '>
+                    <h1 className='font-bold text-lg lg:font-normal '>Anywhere</h1>
+                    <div className='flex text-sm sm:text-lg md:mr-3 lg:justify-evenly flex-1'>
+                        <p className=''>Any week</p>
+                        <p className='text-gray-400'>Any guests</p>
                     </div>
                 </div>
 
                 {/* menu icon */}
-                <div className=' w-3/6 flex justify-end '>
+                <div className=' w-3/6 flex justify-end lg:hidden '>
                     <i className='mx-2 border text-2xl p-2 rounded-full'>
                         <FiMenu />
                     </i>
                 </div>
             </div>
-            <div className='hidden sm:block'>
-
+            <div className='hidden lg:block'>
+                user
             </div>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': { 'min': '640px' },
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      'md': { 'min': '768px' },
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      'lg': { 'min': '1024px' },
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      '2xl': { 'min': '1536px' },
+      // => @media (min-width: 1536px) { ... }
+    },
+
   },
   plugins: [],
 }
